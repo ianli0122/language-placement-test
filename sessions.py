d58b609e23cc8e4b8f5dff114a63df9a36048b53
heolo1 <eldon15563+github@gmail.com>
2024-01-16 23:45:25 -0800

    add session classes and session getter signatures

--- sessions.py
+++ sessions.py
@@ -1,2 +1,24 @@
 import students
 
+class Session:
+	...
+
+class _ReadingSession(Session):
+	...
+
+class _ListeningSession(Session):
+	...
+
+class _SpeakingSession(Session):
+	...
+
+class _WritingSession(Session):
+	...
+
+def is_session_open(id: str) -> bool:
+	...
+
+# gets a new session 
+def open_session(id: str) -> (str, Session):
+
+	...
