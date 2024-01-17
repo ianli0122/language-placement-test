d4da39f33f6f75ba7579d79b0b0eeebee5dde8d3
heolo1 <eldon15563+github@gmail.com>
2024-01-16 23:57:05 -0800

    added sessionid type and added starts of multiple open session checks

--- sessions.py
+++ sessions.py
@@ -1,7 +1,11 @@
+# this is also an imrpoved copy os instances.py
+
 import students
 
 class Session:
 	...
+
+class SessionID(str): ...
 
 class _ReadingSession(Session):
 	...
@@ -16,9 +20,25 @@ class _WritingSession(Session):
 	...
 
 def is_session_open(id: str) -> bool:
-	...
+	return students.get_student_data(id)["in-session"]
 
-# gets a new session 
-def open_session(id: str) -> (str, Session):
+# gets a new session, returns the SessionID (wrapper for a string) and a Session
+# we return SessionID instead of str so we dont get id and session id mixed up
+# if the session id is None, that means either the session is open or they are already finished
+def open_session(id: str) -> (SessionID, Session):
+	if is_session_open(id):
+		return None, None
+	
+	data = students.get_student_data(id)["stage"]
 
-	...
+	match data: # TODO create appropiate constructors
+		case 0:
+			return _ReadingSession()
+		case 1:
+			return _ListeningSession()
+		case 2:
+			return _SpeakingSession()
+		case 3:
+			return _WritingSession()
+		case 4, _:
+			return None, None
