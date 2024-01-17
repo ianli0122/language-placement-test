9e260ae0c50fdf162d083ec14e488bb14b222e32
heolo1 <eldon15563+github@gmail.com>
2024-01-16 19:47:12 -0800

    added student id generation

--- dummy.py
+++ dummy.py
@@ -107,6 +107,10 @@ def _() -> dict:
 def _() -> dict:
 	return {str(i): [f"speaking prompt {j + 1} difficulty={i}" for j in range(3)] for i in range(1, 6)}
 
+@_data_function("student_ids", "Generates student_ids.json, a file containing dummy student IDs which are supposed to emulate IDs of real students taking the test.")
+def _() -> list:
+	return [100_000_000 + i for i in range(100)]
+
 def _gen(key: str) -> None:
 	with open(f"data/{key}.json", "w") as file:
 		print(f"Generating {key}.json...")
