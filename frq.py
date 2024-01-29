0608fae53dedca4573b1720c7c03635f029cb7b5
ianli0122 <ianli0122@gmail.com>
2024-01-28 18:24:07 -0800

    small fix for frqs

--- frq.py
+++ frq.py
@@ -11,10 +11,10 @@ class FRQ:
         self.section = section - 2
     
     def select_prompt(self) -> str:
-        return _questions[self.section][randint(0, 2)]
+        return _questions[self.section][randint(0, len(_questions[self.section]) - 1)]
 
 
-_questions: list[dict] = []
+_questions: list = []
 def question_vars(file: str) -> None:
     with open(f"question_data/{file}.json", 'r', encoding="utf8") as questionFile:
         _questions.append(json.load(questionFile))
