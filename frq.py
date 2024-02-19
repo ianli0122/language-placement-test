06cb91e4ebdb49e6b3b875e708da1098eb131e18
ianli0122 <ianli0122@gmail.com>
2024-02-19 00:21:36 -0800

    such little changes

--- frq.py
+++ frq.py
@@ -17,7 +17,7 @@ class FRQ:
         while self.selectedPrompts[1] == self.selectedPrompts[2]:
             self.selectedPrompts[2] = randint(0, len(_questions[1]) - 1)
     
-    def select_prompt(self, section: int) -> (str, int): # returns prompt, typed prompt index
+    def select_prompt(self, section: int) -> tuple[str, int]: # returns prompt, typed prompt index
         section -= 2
         if section == 0:
             return _questions[section][self.selectedPrompts[section]], -1
